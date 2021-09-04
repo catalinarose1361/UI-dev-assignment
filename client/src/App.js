b02ab1d50e039a16807f35bd95b69859fad7a963
Catalina Williams <webdevcatalina@gmail.com>
2021-09-03 18:29:13 -0600

    added switch that changes application from dark mode to light mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState } from 'react';
 
 import Locations from './components/Locations';
 
@@ -6,19 +6,23 @@ import Users from './components/Users';
 
 import { Grid, Typography, Paper, Button } from '@material-ui/core';
 import {ThemeProvider, createTheme} from '@material-ui/core/styles';
-
+import Switch from '@material-ui/core/Switch';
 function App() {
+
+
+  const [darkMode, setDarkMode] = useState(false)
   const theme = createTheme({
     palette: {
-      type: "dark"
+      type: darkMode ? "dark" : "light",
     }
   })
+
   return (
   
     <ThemeProvider theme={theme}>
-      <Paper>
+      <Paper style={{ height: "100vh"}}>
       <div>
-      
+      <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)} name="checkedA" />
       <Users />
 
       <Locations />
